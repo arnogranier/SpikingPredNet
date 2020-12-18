4f8d4bd673230ae4ec75a518fcdab9ba65bc3736
ArnoGranier <arno@ini.uzh.ch>
2020-12-18 10:34:11 +0100

    merge

--- simple.py
+++ simple.py
@@ -15,12 +15,9 @@ interlNPE = neurons(5, name='interlNPE')
 rates = b2.TimedArray(np.tile(np.array([[80, 0], [0, 80]]), (10,1))*b2.Hz, dt=1000.*b2.ms)
 lIR = b2.PoissonGroup(5, rates='rates(t)', name='lIR')
 
-<<<<<<< HEAD
 b2.Synapses(hIR, lPPE)
 b2.Synapses(hIR, interlNPE)
 b2.Synapses(lPPE, interhIR)
 b2.Synapses(lNPE, hIR)
 b2.Synapses(lIR, lNPE)
 b2.Synapses(lIR, interlPPE)
-=======
->>>>>>> 2ec040a9b685d425cb582d23c36c561ea95b51ee
