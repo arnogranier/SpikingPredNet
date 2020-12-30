cb39e7e1a8755c711756cd089a146c032fd55880
ArnoGranier <arno@ini.uzh.ch>
2020-12-30 13:03:24 +0100

    simple 1 2 3

--- modules.py
+++ modules.py
@@ -2,7 +2,7 @@ import brian2 as b2
 from neuron_model import neurons, synapses
         
 class Area:
-    def __init__(self, N, name, net, wINHIPE, wEXCIPE, wINHIIR=None, wEXCIIR=None, IRPoisson=False, recordspikes=False):
+    def __init__(self, N, name, net, wINHIPE, wEXCIPE, wINHIIR=None, wEXCIIR=None, IRPoisson=False, recordspikes=False, onlyIR=False):
         self.net = net
         self.name = name
         self.N = N
@@ -14,27 +14,30 @@ class Area:
             IR = b2.NeuronGroup(N, 'rates : Hz', threshold='rand()<rates*dt',
                                 name=name+'_IR', refractory=3*b2.ms)
             net.add(IR)
-        PPE = neurons(N, name=name+'_PPE', net=net)
-        interPPE = neurons(N, name=name+'_interPPE', behavior='i', net=net)
-        NPE = neurons(N, name=name+'_NPE', net=net)
-        interNPE = neurons(N, name=name+'_interNPE', behavior='i', net=net)
-        
-        synapses(interPPE, PPE, 'i==j', wINHIPE, net)
-        synapses(interNPE, NPE, 'i==j', wINHIPE, net)
-        synapses(IR, interPPE, 'i==j', 35, net, delay=.1*b2.ms)
-        synapses(IR, NPE, 'i==j', wEXCIPE, net, delay=.1*b2.ms)
         if not IRPoisson:
-            synapses(PPE, interIR, 'i==j', 35, net)
-            synapses(NPE, IR, 'i==j', wEXCIPE, net)
             synapses(interIR, IR, 'i==j', wINHIIR, net)
+            synapses(IR, IR, 'i==j', 35, net, delay=20*b2.ms)
+        if not onlyIR:
+            PPE = neurons(N, name=name+'_PPE', net=net)
+            interPPE = neurons(N, name=name+'_interPPE', behavior='i', net=net)
+            NPE = neurons(N, name=name+'_NPE', net=net)
+            interNPE = neurons(N, name=name+'_interNPE', behavior='i', net=net)
+            synapses(interPPE, PPE, 'i==j', wINHIPE, net)
+            synapses(interNPE, NPE, 'i==j', wINHIPE, net)
+            synapses(IR, interPPE, 'i==j', 35, net, delay=.1*b2.ms)
+            synapses(IR, NPE, 'i==j', wEXCIPE, net, delay=.1*b2.ms)
+            if not IRPoisson:
+                synapses(PPE, IR, 'i==j', wEXCIIR, net)
+                synapses(NPE, interIR, 'i==j', 35, net)
 
         if recordspikes:
             IRrecord = b2.SpikeMonitor(net[name+'_IR'], name=name+'_IR_RECORD')
             net.add(IRrecord)
-            PPErecord = b2.SpikeMonitor(net[name+'_PPE'], name=name+'_PPE_RECORD')
-            net.add(PPErecord)
-            NPErecord = b2.SpikeMonitor(net[name+'_NPE'], name=name+'_NPE_RECORD')
-            net.add(NPErecord)
+            if not onlyIR:
+                PPErecord = b2.SpikeMonitor(net[name+'_PPE'], name=name+'_PPE_RECORD')
+                net.add(PPErecord)
+                NPErecord = b2.SpikeMonitor(net[name+'_NPE'], name=name+'_NPE_RECORD')
+                net.add(NPErecord)
         
     def set_rates(self, rate_var_name):
         self.net[self.name+'_IR'].run_regularly('rates = %s(t,i)'%rate_var_name, 1*b2.second)
@@ -56,8 +59,8 @@ def connect(a1, a2, W, wEXCIPE, wEXCIIR=None, plastic=False):
     synapses(net[a1.name+'_IR'], net[a2.name+'_PPE'], (sources, targets), wEXCIPE, net)
     synapses(net[a1.name+'_IR'], net[a2.name+'_interNPE'], (sources, targets), 35, net)
     if not a1.IRPoisson:
-        synapses(net[a2.name+'_PPE'], net[a2.name+'_interIR'], (targets, sources), 35, net)
-        synapses(net[a2.name+'_NPE'], net[a2.name+'_IR'], (targets, sources), wEXCIIR, net)
+        synapses(net[a2.name+'_PPE'], net[a1.name+'_interIR'], (targets, sources), 35, net)
+        synapses(net[a2.name+'_NPE'], net[a1.name+'_IR'], (targets, sources), wEXCIIR, net)
     
     
     
--- simple.py
+++ simple.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 from neuron_model import C, gL, EL, VT, DeltaT, Vcut
 from neuron_model import neurons, synapses
 from plots import rplots, vplots
-from area import Area, connect 
+from modules import Area, connect 
 
 def exp1():
-    activationLOW = np.array([[0, 0, 1], [0, 0, 1], [1, 0, 1], [0, 1, 0]])
-    activationHIGH = np.array([[0, 1], [1, 0], [0, 1], [0, 1]])
+    activationLOW = np.array([[1, 0, 0], [1, 0, 0], [1, 0, 1], [1, 1, 0]])
+    activationHIGH = np.array([[1, 0], [0, 1], [1, 0], [1, 0]])
     timedRatesLOW = b2.TimedArray(65*activationLOW*b2.Hz, dt=1*b2.second)
     timedRatesHIGH = b2.TimedArray(65*activationHIGH*b2.Hz, dt=1*b2.second)
     net = b2.Network()
-    wINHIPE, wEXCIPE = -20, 12
+    wINHIPE, wEXCIPE = -25, 12
     W = np.array([[1, 0, 1], 
                   [0, 1, 1]])
     LOW = Area(3, 'LOW', net, wINHIPE, wEXCIPE, IRPoisson=True, recordspikes=True)
@@ -31,10 +31,36 @@ def exp1():
     plt.show()
 
 def exp2():
-    pass 
+    activationLOW = np.array([[1, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 1]])
+    timedRatesLOW = b2.TimedArray(65*activationLOW*b2.Hz, dt=1*b2.second)
+    net = b2.Network()
+    wINHIPE, wEXCIPE = -25, 12
+    wINHIIR, wEXCIIR = -20, 25
+    W = np.array([[1, 0, 1], 
+                  [0, 1, 1]])
+    LOW = Area(3, 'LOW', net, wINHIPE, wEXCIPE, IRPoisson=True, recordspikes=True)
+    HIGH = Area(2, 'HIGH', net, wINHIPE, wEXCIPE, wINHIIR, wEXCIIR, onlyIR=True, recordspikes=True)
+    LOW.set_rates('timedRatesLOW')
+    connect(HIGH, LOW, W, wEXCIPE, wEXCIIR, plastic=False)
+    net.run(4*b2.second)
+    rplots(LOW['IR'], HIGH['IR'], LOW['PPE'], LOW['NPE'])
+    plt.show()
 
 def exp3():
-    pass 
+    activationHIGH = np.array([[1, 0], [0, 1], [1, 0], [0, 1]])
+    timedRatesHIGH = b2.TimedArray(65*activationHIGH*b2.Hz, dt=1*b2.second)
+    net = b2.Network()
+    wINHIPE, wEXCIPE = -25, 12
+    wINHIIR, wEXCIIR = -20, 25
+    W = np.array([[1, 0, 1], 
+                  [0, 1, 1]])
+    LOW = Area(3, 'LOW', net, wINHIPE, wEXCIPE, wINHIIR, wEXCIIR, recordspikes=True)
+    HIGH = Area(2, 'HIGH', net, wINHIPE, wEXCIPE, IRPoisson=True, onlyIR=True, recordspikes=True)
+    HIGH.set_rates('timedRatesHIGH')
+    connect(HIGH, LOW, W, wEXCIPE, wEXCIIR, plastic=False)
+    net.run(4*b2.second)
+    rplots(LOW['IR'], HIGH['IR'], LOW['PPE'], LOW['NPE'])
+    plt.show()
 
 def exp4():
     pass 
@@ -42,7 +68,5 @@ def exp4():
 def exp5():
     pass 
 
-def exp6():
-    pass
 if __name__ == "__main__":
-    exp1()
+    exp2()
