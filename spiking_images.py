7bbc268b6400ebe335479e679546911e12dcf8ce
ArnoGranier <arno@ini.uzh.ch>
2021-01-02 11:01:25 +0100

    comment and docstring spiking_images

--- spiking_images.py
+++ spiking_images.py
@@ -7,27 +7,37 @@ import pickle
 
 def PoissonImages(imgfolder:str, presentation_time:float,
                   resize:tuple=None, contrast_multiplier:float=1,
-                  imgmode:str=None):
-    """[summary]
+                  imgmode:str='L',value_to_rate_coeff:float=0.25):
+    """Load each images in imgfolder onto a Poisson neural population, 
+    changing image every presentation_time ms
 
     Args:
-        imgfolder (str): [description]
-        presentation_time (float): [description]
-        resize (tuple, optional): [description]. Defaults to None.
-        contrast_multiplier (float, optional): [description]. Defaults to 1.
-        imgmode (str, optional): [description]. Defaults to None.
+        imgfolder (str): location of images
+        presentation_time (float): presentation time of each image in ms
+        resize (tuple, optional): Image size. If None no resize is performed.
+                                  Defaults to None.
+        contrast_multiplier (float, optional): Controls contrast of images. >1 
+                                               is higher contrast. Defaults to 
+                                               1.
+        imgmode (str, optional): Color encoding (B&W, grayscale, RGB, etc..). 
+                                 This is a parameter passed to PIL.Image.convert
+                                 Defaults to grayscale.
+        value_to_rate_coeff (float, optional): coeff to pass from pixel value 
+                                               (0->255) to Poisson firing rate. 
+                                               Defaults to 0.25.
 
     Returns:
-        [type]: [description]
-        [type]: [description]
+        brian2.TimedArray: A TimedArray of firing rates representing images
+        brian2.NeuronGroup: A Poisson neural population following these
+                            firing rates
     """
     
     arrays = list()
     for filename in os.listdir(sys.path[0]+imgfolder):
-        if filename.endswith(".jpg") or filename.endswith(".png") \\
+        if filename.endswith(".jpg") or filename.endswith(".png") \
          or filename.endswith(".jpeg"):
             im = PIL.Image.open(os.path.join(sys.path[0]+imgfolder, filename))
-            im = im.convert(imgmode) if imgmode is not None else im.convert('L')
+            im = im.convert(imgmode)
             if resize is not None:
                 im = im.resize(resize)
             if contrast_multiplier != 1:
@@ -37,7 +47,8 @@ def PoissonImages(imgfolder:str, presentation_time:float,
         else:
             continue
     values = np.vstack(arrays)
-    images = b2.TimedArray(values*b2.Hz/4, presentation_time*b2.ms)
+    images = b2.TimedArray(values*b2.Hz*value_to_rate_coeff,
+                           presentation_time*b2.ms)
     N = len(values[0])
     group = b2.NeuronGroup(N, 'rates : Hz', threshold='rand()<rates*dt')
     group.run_regularly('rates = images(t,i)', presentation_time*b2.ms)
@@ -46,19 +57,36 @@ def PoissonImages(imgfolder:str, presentation_time:float,
 def PoissonMNIST(presentation_time:float=1000, N:int=60000, resize:tuple=None,
                  contrast_multiplier:float=1, imgmode:str=None,
                  value_to_rate_coeff:float=0.25, name:str='PoissonMNIST'):
-    """[summary]
+    """Load MNIST dataset onto a Poisson neural population changing image
+    every presentation_time. Necessit of pickled file containing mnist_imgs the
+    flatten pixel values of MNIST digits and mnist_labels the MNIST labels
+    (that could be made more practical)
 
     Args:
-        presentation_time (float, optional): [description]. Defaults to 1000.
-        N (int, optional): [description]. Defaults to 60000.
-        resize (tuple, optional): [description]. Defaults to None.
-        contrast_multiplier (float, optional): [description]. Defaults to 1.
-        imgmode (str, optional): [description]. Defaults to None.
-        value_to_rate_coeff (float, optional): [description]. Defaults to 0.25.
-        name (str, optional): [description]. Defaults to 'PoissonMNIST'.
+        presentation_time (float, optional): presentation time of each image in 
+                                             ms. Defaults to 1000.
+        N (int, optional): Number of images to load. Defaults to 60000.
+        resize (tuple, optional): Image size. If None no resize is performed.
+                                  Defaults to None.
+        contrast_multiplier (float, optional): Controls contrast of images. >1 
+                                               is higher contrast. Defaults to 
+                                               1.
+        imgmode (str, optional): Color encoding (B&W, grayscale, RGB, etc..). 
+                                 This is a parameter passed to PIL.Image.convert
+                                 Defaults to grayscale.
+        value_to_rate_coeff (float, optional): coeff to pass from pixel value 
+                                               (0->255) to Poisson firing rate. 
+                                               Defaults to 0.25.
+        name (str, optional): Name of the neural population. Defaults to 
+                              'PoissonMNIST'.
 
     Returns:
-        [type]: [description]
+        brian2.TimedArray: A TimedArray of firing rates representing MNIST
+                           digits
+        brian2.NeuronGroup: A Poisson neural population following these
+                            firing rates
+        list of int: Associated MNIST labels
+        float * brian2.ms: Total simulation time for each image to be presented
     """
     
     f = open(sys.path[0]+"/mnist", "rb")
