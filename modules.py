3549cb5724b9a92f051d35b5598b8a2a3bf892ee
ArnoGranier <arno@ini.uzh.ch>
2021-01-02 12:03:18 +0100

    comment and docstring modules

--- modules.py
+++ modules.py
@@ -5,30 +5,47 @@ from neuron_model import neurons, synapses
         
 
 class Area:
-    """[summary]
+    """Main class representing a spiking predictive processing model of a 
+    cortical area. Holds internal representation (IR) and prediction error (PPE 
+    and NPE) neural populations, and all local connections between them.
 
     Args:
-        N (int): [description]
-        name (str): [description]
-        net (b2.Network): [description]
-        wINHIPE (float): [description]
-        wEXCIPE (float): [description]
-        wINHIIR (float, optional): [description]. Defaults to 0..
-        wEXCIIR (float, optional): [description]. Defaults to 0..
-        IRPoisson (bool, optional): [description]. Defaults to False.
-        IRSet (bool, optional): [description]. Defaults to False.
-        recordspikes (bool, optional): [description]. Defaults to False.
-        onlyIR (bool, optional): [description]. Defaults to False.
-        lateralplasticity (bool, optional): [description]. Defaults to False.
-        SetSpikes (list, optional): [description]. Defaults to list().
-        wmax (float, optional): [description]. Defaults to 35..
+        N (int): Number of neurons
+        name (str): Name of the area
+        net (b2.Network): brian2.Network in which to add population and synapses
+        wINHIPE (float): Weight of synapses inhibiting PE populations
+        wEXCIPE (float): Weight of synapses exciting PE populations
+        wINHIIR (float, optional): Weight of synapses inhibiting the IR
+                                   population. Defaults to 0..
+        wEXCIIR (float, optional): Weight of synapses exciting the IR
+                                   population. Defaults to 0..
+        IRPoisson (bool, optional): If True then the IR population is defined as
+                                    following Poisson spike trains of rates
+                                    defined by calling Area.set_rates. Defaults
+                                    to False.
+        IRSet (bool, optional): If True then the IR population is defined as
+                                following deterministic spike trains defined by
+                                calling Area.set_spikes. Defaults to False.
+        recordspikes (bool, optional): If True then spike of the 3 populations
+                                       are recorded through b2.SpikeMonitor,
+                                       else not. Defaults to False.
+        onlyIR (bool, optional): If True then skip the creation of PE
+                                 populations. Defaults to False.
+        lateralplasticity (bool, optional): If True then add lateral plastic
+                                            synapses to the IR populations.
+                                            Defaults to False.
+        SetSpikes (list, optional): List of spikes to force in the IR
+                                    population. The list should contain tuples
+                                    (index of neuron, time of spike). Defaults
+                                    to [].
+        wmax (float, optional): Max weight. Defaults to 35..
     """
     
     def __init__(self, N:int, name:str, net:b2.Network, wINHIPE:float,
                  wEXCIPE:float, wINHIIR:float=0., wEXCIIR:float=0.,
                  IRPoisson:bool=False, IRSet:bool=False,
                  recordspikes:bool=False, onlyIR:bool=False,
-                 lateralplasticity:bool=False, SetSpikes:list=list(),
+                 lateralplasticity:bool=False, SetSpikes:list=[],
                  wmax:float=35.):
         self.net = net
         self.name = name
@@ -81,31 +98,37 @@ class Area:
             synapses(INITSG, IR, (0, index), wmax, net)
         
     def set_rates(self, rate_var_name:str):
-        """[summary]
+        """Set the rates of the Poisson internal representation population (if 
+        it is indeed supposed to be Poisson).
+        Since brian2genn does not support brian2.TimedArray as model variable,
+        we pass the name of the brian2.TimedArray representing firing rates and
+        call NeuronGroup.run_regularly to change firing rates every second. 
+        This is a dirty workaround, but it works.
 
         Args:
-            rate_var_name (str): [description]
+            rate_var_name (str): Name of the brian2.TimedArray variable
         """
         assert self.IRPoisson
         IR = self.net[self.name+'_IR']
         IR.run_regularly('rates = %s(t,i)'%rate_var_name, 1*b2.second)
         
     def set_spikes(self, indices:(list, np.ndarray), times:(list, np.ndarray)):
-        """[summary]
+        """Set the spikes of internal representations if these are set to be 
+        a predefined deterministic spike train (brian2.SpikeGeneratorGroup)
 
         Args:
-            indices (list, np.ndarray): [description]
-            times (list, np.ndarray): [description]
+            indices (list, np.ndarray): indices of firing neurons
+            times (list, np.ndarray): times of spikes
         """
         assert self.IRSet
         self.net[self.name+'_IR'].set_spikes(indices, times)    
     
-    def record_variable(self, pop:str, name:str):
-        """[summary]
+    def record_variables(self, pop:str, name:str):
+        """Record variables from population pop with a brian2.StateMonitor
 
         Args:
-            pop (str): [description]
-            name (str): [description]
+            pop (str): Name of the population to record from
+            name (str): Name of the brian2.StateMonitor
         """
         record = b2.StateMonitor(self.net[self.name+'_'+pop], 'vm',
                                  record=True,
@@ -113,13 +136,15 @@ class Area:
         self.net.add(record)
         
     def __getitem__(self, key:str):
-        """[summary]
-
+        """Get a Monitor with the Area[] syntax. SpikeMonitors are accessed
+        through the keywords 'IR', 'PPE' and 'NPE'. StateMonitors are 
+        accessed with their names.
+        
         Args:
-            key (str): [description]
+            key (str): Name of monitor
 
         Returns:
-            [type]: [description]
+            brian2.SpikeMonitor or brian2.StateMonitor: The monitor
         """
         return self.net[self.name+'_'+key+'_RECORD']
         
@@ -127,18 +152,24 @@ class Area:
 def connect(a1:Area, a2:Area, W:np.ndarray, wEXCIPE:float, wEXCIIR:float=0.,
             plastic:bool=False, onlyNPE:bool=False, onlyPPE:bool=False,
             wmax:float=35):
-    """[summary]
+    """Connect two Area. a1 sends predictions to a2, and a2 sends back
+    prediction errors to a1.
 
     Args:
-        a1 (Area): [description]
-        a2 (Area): [description]
-        W (np.ndarray): [description]
-        wEXCIPE (float): [description]
-        wEXCIIR (float, optional): [description]. Defaults to 0..
-        plastic (bool, optional): [description]. Defaults to False.
-        onlyNPE (bool, optional): [description]. Defaults to False.
-        onlyPPE (bool, optional): [description]. Defaults to False.
-        wmax (float, optional): [description]. Defaults to 35.
+        a1 (Area): Name of the higher population
+        a2 (Area): Name of the lower population
+        W (np.ndarray): prediction weight matrix
+        wEXCIPE (float): Weight of synapses exciting lower PE populations
+        wEXCIIR (float, optional): Weight of synapses exciting higher IR
+                                   population. Defaults to 0..
+        plastic (bool, optional): If True then prediction weights are plastic
+                                  and learning of predictions occur. Defaults
+                                  to False.
+        onlyNPE (bool, optional): If True only connect negative PE population.
+                                  Defaults to False.
+        onlyPPE (bool, optional): If True only connect posistive PE population.
+                                  Defaults to False.
+        wmax (float, optional): Max weight. Defaults to 35.
     """
     assert a1.net == a2.net 
     assert W.shape == (a1.N, a2.N)
