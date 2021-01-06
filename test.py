4d9a1989e072b6dda790b483973bd34e00000d34
Arno Granier <arno@ini.uzh.ch>
2021-01-06 20:42:22 +0100

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import brian2 as b2 
-import numpy as np 
-
-
-a = b2.SpikeGeneratorGroup(1, [0], [50,]*b2.ms)
-b = b2.NeuronGroup(3, model='vm:volt')
-c = b2.NeuronGroup(3, model='vm:volt')
-s = b2.Synapses(a, b, model='Wf:1', on_pre='vm+=10*int(Wf>.5)*mV')
